3eda349c650e522ca97edabc98073770322f8c9f
mikishlep <gleb.tseilonsky@yandex.ru>
2025-07-27 18:01:21 +0500

    Добавлены начальные файлы проекта: конфигурация Vite, компоненты Vue, стили и документация. Создан .gitignore для исключения временных файлов и директорий.

---
+++ catchEggsApp/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainMenu from './components/MainMenu.vue';
+</script>
+
+<template>
+  <MainMenu />
+</template>
+
+<style scoped>
+
+</style>
---
+++ catchEggsApp/src/components/MainMenu.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>LOL</h1>
+</template>
+
+<style scoped>
+
+</style>
