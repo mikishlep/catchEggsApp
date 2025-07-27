f40cfc10b913e3fa829ec63c367a847218be600a
mikishlep <gleb.tseilonsky@yandex.ru>
2025-07-27 21:24:23 +0500

    Обновлен кэш хостинга Firebase, добавлен импорт onMounted в компонент App.vue и стили для заголовка в компоненте MainMenu.vue.

--- catchEggsApp/src/App.vue
+++ catchEggsApp/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { onMounted } from 'vue';
 import MainMenu from './components/MainMenu.vue';
 
 onMounted(() => {
--- catchEggsApp/src/components/MainMenu.vue
+++ catchEggsApp/src/components/MainMenu.vue
@@ -7,5 +7,10 @@
 </template>
 
 <style scoped>
-
+h1 {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>
